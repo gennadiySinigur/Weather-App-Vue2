d3dd1d5392d624e9135feedfd789afe86784bac5
gennadiySinigur <gennadiysinigur@gmail.com>
2022-05-16 22:34:38 +0300

    Added custom styles to alertForUser

--- src/utils/alertForUser.js
+++ src/utils/alertForUser.js
@@ -2,13 +2,20 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 import Swal from 'sweetalert2';
 
 export const showAlertForUser = (errorMessage) => {
+  const errorText = document.createElement('p');
+  const titleText = document.createElement('h4');
+
+  errorText.innerText = `${errorMessage}`;
+  titleText.innerText = 'Error!';
+
+  errorText.style.fontFamily = 'sans-serif';
+  titleText.style.fontFamily = 'sans-serif';
+
   return Swal.fire({
-    title: 'Error!',
+    title: titleText,
     icon: 'error',
     showConfirmButton: true,
-    text: errorMessage,
-    customClass: {
-      content: 'red',
-    },
+    html: errorText,
+    width: '470px',
   });
 };
